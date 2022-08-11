707d83c9f8435313c02ca1f5ac6d7d2ffd49d53f
Tudor <tudor.stoiu@gmail.com>
2022-08-10 21:39:22 -0700

    fix .gitignore

---
+++ nvim/.config/nvim/lua/tstoiu17/keybinds.lua
@@ -0,0 +1,60 @@
+-- REMAP SHORTCUTS
+local function map(mode, lhs, rhs)
+    vim.keymap.set(mode, lhs, rhs, { noremap = true, silent = true })
+end
+
+-- NORMAL MODE REMAP CALLER
+local function nmap(lhs, rhs)
+    map("n", lhs, rhs)
+end
+
+-- VISUAL MODE REMAP CALLER
+local function vmap(lhs, rhs)
+    map("v", lhs, rhs)
+end
+
+-- INSERT MODE REMAP CALLER
+local function imap(lhs, rhs)
+    map("i", lhs, rhs)
+end
+
+-- MOVEMENTS
+
+-- move between splits
+nmap("<C-h>", ":wincmd h<CR>")
+nmap("<C-j>", ":wincmd j<CR>")
+nmap("<C-k>", ":wincmd k<CR>")
+nmap("<C-l>", ":wincmd l<CR>")
+
+-- New tab
+nmap('te', ':tabedit')
+
+-- Split window
+nmap('ss', ':split<Return><C-w>w')
+nmap('sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+nmap('sh', '<C-w>h')
+nmap('sk', '<C-w>k')
+nmap('sj', '<C-w>j')
+nmap('sl', '<C-w>l')
+
+-- ACTIONS
+
+nmap("<leader>e", ":Ex<CR>")
+
+-- EDITING
+
+-- increment/decrement
+nmap("+", "<C-a>")
+vmap("+", "<C-a>")
+nmap("-", "<C-x>")
+vmap("-", "<C-x>")
+
+-- yanking
+nmap("Y", "y$")
+vmap("<C-C>", '"+y')
+
+-- selection
+nmap("<C-a>", "ggVG")
+
---
+++ nvim/.config/nvim/lua/tstoiu17/packer.lua
@@ -0,0 +1,10 @@
+return require("packer").startup(function()
+    use "wbthomason/packer.nvim"    
+
+    -- Section Title
+
+    -- Themes
+    use "folke/tokyonight.nvim" 
+    use "gruvbox-community/gruvbox"
+    use 'rockerBOO/boo-colorscheme-nvim'
+end)
