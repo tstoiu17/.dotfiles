4307df1f597119ede580819a36adc3bea6f2186c
Tudor <tudor.stoiu@gmail.com>
2022-08-26 08:26:14 -0700

    nvim: disable swapfile and backup

--- nvim/.config/nvim/lua/tstoiu17/settings.lua
+++ nvim/.config/nvim/lua/tstoiu17/settings.lua
@@ -14,8 +14,8 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
---vim.opt.swapfile = false
---vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.backup = false
 --vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 --vim.opt.undofile = true
 
