af170a150fd55d3ca72eeabbec13a8108beb9bae
Tudor <tudor.stoiu@gmail.com>
2022-08-29 21:51:55 -0700

    lvim: set relativenumber

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -7,6 +7,7 @@ a global executable or a path to
 an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+vim.opt.relativenumber = true
 
 -- general
 lvim.log.level = "warn"
