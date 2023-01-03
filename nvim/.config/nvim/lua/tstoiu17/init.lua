485e544dd7ced23dce86c0f2df04000e1e2ece8d
Tudor <tudor.stoiu@gmail.com>
2023-01-03 12:42:35 -0800

    add highlight on yank

--- nvim/.config/nvim/lua/tstoiu17/init.lua
+++ nvim/.config/nvim/lua/tstoiu17/init.lua
@@ -1,3 +1,14 @@
 require("tstoiu17.plugins") -- needs to required before the others
 require("tstoiu17.settings")
 require("tstoiu17.keymaps")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    group = vim.api.nvim_create_augroup('HighlightYank', {}),
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
