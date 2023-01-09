dd4c26837d6bc05f0def5e1dbd23cc647ef7b383
Tudor <tudor.stoiu@gmail.com>
2023-01-08 17:16:32 -0800

    organize harpoon keymaps

--- nvim/.config/nvim/after/plugin/harpoon.lua
+++ nvim/.config/nvim/after/plugin/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[A]dd file - Harpoon" })
-vim.keymap.set("n", "<leader>H", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
+vim.keymap.set("n", "<leader>Ha", mark.add_file, { desc = "[A]dd file - Harpoon" })
+vim.keymap.set("n", "<leader>HH", ui.toggle_quick_menu, { desc = "Toggle Harpoon Menu" })
 
 vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon 1" })
 vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon 2" })
