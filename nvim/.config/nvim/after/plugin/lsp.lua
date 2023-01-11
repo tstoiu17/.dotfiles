03f4ca9cbd05bb99e5422ba4da1e257827de78db
Tudor <tudor.stoiu@gmail.com>
2023-01-10 18:12:10 -0800

    update lsp keymaps

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -2,4 +2,13 @@ require("neodev").setup {}
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+  vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+end)
+
 lsp.setup()
+
+vim.diagnostic.config({ virtual_text = true })
