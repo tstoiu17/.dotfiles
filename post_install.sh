de0813ded82cb8998ba98b3b18c33eda907e3b90
Tudor <tudor.stoiu@gmail.com>
2023-07-06 23:35:00 -0700

    update post install script

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+pacman -S --needed base-devel git neovim zsh stow
 username="tudor"
 useradd --create-home --groups wheel --shell /usr/bin/zsh $username
 passwd $username
 echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 # install paru
-pacman -S --needed base-devel git
 cd /home/$username
 sudo -u $username git clone https://aur.archlinux.org/paru.git
 cd paru
